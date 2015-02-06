e47720ec91a408530feeaeaba9a5031366ffdb89
Mário abreu <mjm.abreu@gmail.com>
2015-02-06 12:11:28 +0000

    Added more changes to the html + css + javascript

---
+++ basic-introduction-design/pmail/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {     
+    $('#user-logo').hover(function(){     
+        $('#dropdown-menu').removeClass('');    
+    },
+    function(){    
+        $('#dropdown-menu').addClass('');     
+    });
+}); 
